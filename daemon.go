ffeacc9cfbd45195194fad2985abb93dee62cd88
Elliot Wright <wright.elliot@gmail.com>
2026-07-09 11:56:13 +0100

    Big shuffle around of RunE so it no longer calls os.Exit. No more printing to stdout. Less leaky stuff. InitializableRoutine makes that whole concept a lot clearer and cleaner too

--- daemon.go
+++ daemon.go
@@ -11,6 +11,11 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+var (
+	ErrGracePeriodExceeded = errors.New("daemon: grace period exceeded")
+	ErrInterrupted         = errors.New("daemon: interrupted")
+)
+
 // Run is a helper around RunE which returns an int expected to be used as an exit code passed to
 // os.Exit.
 func Run(gracePeriod time.Duration, routines ...Routine) int {
@@ -21,20 +26,22 @@ func Run(gracePeriod time.Duration, routines ...Routine) int {
 }
 
 // RunE starts the given routines, blocking until they are terminated, or error.
+//
 // If the process receives an interrupt signal, the context passed to all routines will be cancelled
-// to signal for them to being gracefully shutting down. If the routines all exit gracefully, or if
-// the grace period is reached, the program will exit using os.Exit.
+// to signal for them to begin gracefully shutting down. If no further signals are received, but the
+// routines don't terminate within the provided grace period, an ErrGracePeriodExceeded error will
+// be returned. If a second signal is received RunE will attempt to return immediately, returning an
+// ErrInterrupted. In this scenario, if the program doesn't exit, some resources may leak.
 //
-// The first error encountered is returned, except for context-related errors.
+// The first error encountered is returned, except for context.Canceled errors.
 //
-// Routines are started in the order they're given. If they utilize the given `initialized` channel
-// then they will also wait for the previous routine to signal that it has started before moving on
-// to the next routine. Handy for doing things like warming up caches or populating in-memory
-// stores.
+// Routines are started in the order they're given. If a Routine is an InitializableRoutine then
+// its Initialize method will be called before Run, and before moving onto starting the next
+// Routine. If any Routine fails to initialize, the rest of the routines will not be started.
 func RunE(gracePeriod time.Duration, routines ...Routine) error {
-	// Channel to avoid leaking goroutines from this function.
-	done := make(chan struct{})
-	defer close(done)
+	if len(routines) == 0 {
+		panic("daemon: no routines provided")
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -43,54 +50,60 @@ func RunE(gracePeriod time.Duration, routines ...Routine) error {
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	defer signal.Stop(sigCh)
 
+	errCh := make(chan error, len(routines))
+
 	go func() {
-		select {
-		case <-done:
-			return
-		case <-sigCh:
-			cancel()
+		defer close(errCh)
+
+		g, ctx := errgroup.WithContext(ctx)
+
+		var initErr error
+		for _, routine := range routines {
+			if initializable, ok := routine.(InitializableRoutine); ok {
+				if err := initializable.Initialize(ctx); err != nil {
+					initErr = err
+					cancel()
+					break
+				}
+			}
+
+			g.Go(func() error {
+				return routine.Run(ctx)
+			})
 		}
 
-		select {
-		case <-done:
+		err := g.Wait()
+
+		// For the following error checks, we don't propagate context.Canceled because it's expected to
+		// be cancelled. It might've been cancelled by the previous routing failing to initialize too.
+
+		// If we got an initialization error, we'll report that over anything else, as it's probably the
+		// most relevant one to see.
+		if initErr != nil && !errors.Is(initErr, context.Canceled) {
+			errCh <- initErr
 			return
-		case <-time.After(gracePeriod):
-		case <-sigCh:
 		}
 
-		os.Exit(1)
+		// Otherwise, we check if there was an error in-flight with any of the routines.
+		if err != nil && !errors.Is(err, context.Canceled) {
+			errCh <- err
+			return
+		}
 	}()
 
-	g, ctx := errgroup.WithContext(ctx)
-
-	var initErr error
-	for _, routine := range routines {
-		initialized := make(chan error)
-		g.Go(func() error {
-			return routine.Run(ctx, initialized)
-		})
-		// Wait for this routine to start before moving on. Capture failures.
-		if initErr = <-initialized; initErr != nil {
-			cancel()
-			break
-		}
-	}
-
-	err := g.Wait()
-
-	// For the following error checks, we don't propagate context.Canceled because it's expected to
-	// be cancelled. It might've been cancelled by the previous routing failing to initialize too.
-
-	// If we got an initialization error, we'll report that over anything else, as it's probably the
-	// most relevant one to see.
-	if initErr != nil && !errors.Is(initErr, context.Canceled) {
-		return initErr
-	}
-
-	// Otherwise, we check if there was an error in-flight with any of the routines.
-	if err != nil && !errors.Is(err, context.Canceled) {
+	select {
+	case err := <-errCh:
 		return err
+	case <-sigCh:
+		cancel()
 	}
 
-	return nil
+	select {
+	case err := <-errCh:
+		return err
+	case <-time.After(gracePeriod):
+		return ErrGracePeriodExceeded
+	case <-sigCh:
+		return ErrInterrupted
+	}
 }
--- routine.go
+++ routine.go
@@ -8,30 +8,14 @@ type Routine interface {
 	// Run attempts to start this Routine. This function should block until the Routine has ended.
 	// The returned error should be nil if the Routine ended successfully, or an error if it ended
 	// with an error.
-	//
-	// The initialized channel may be used to signal the Routine has finished preparing to start,
-	// and is moving onto starting. Not all routines will need to take advantage of this, but this
-	// channel MUST be closed by the routine, or further threads will not be run. This is useful for
-	// threads which need to prepare something before starting, like an in-memory store, or doing
-	// some kind of cache-warming.
-	Run(ctx context.Context, initialized chan<- error) error
+	Run(ctx context.Context) error
 }
 
-// RoutineFunc wraps a function that matches the signature of Routine.Run into a Routine.
-// Useful for adapting compatible functions into a Routine.
-type RoutineFunc func(ctx context.Context, initialized chan<- error) error
-
-// Run this RoutineFunc.
-func (fn RoutineFunc) Run(ctx context.Context, initialized chan<- error) error {
-	return fn(ctx, initialized)
-}
-
-// SimpleRoutineFunc wraps a function that has a simpler signature than Routine.Run into a Routine.
-// Useful for simple routines, and adapting compatible functions into a Routine.
-type SimpleRoutineFunc func(ctx context.Context) error
-
-// Run this SimpleRoutineFunc.
-func (fn SimpleRoutineFunc) Run(ctx context.Context, initialized chan<- error) error {
-	close(initialized) // Unused in this case, but we this means we can't guarantee ordering.
-	return fn(ctx)
+// InitializableRoutine is a specialized Routine that has an initialization step. Handy for when
+// your Routine needs to do some work before starting, like populating in-memory stores, warming up
+// caches, etc.
+type InitializableRoutine interface {
+	Routine
+	// Initialize attempts to initialize this Routine.
+	Initialize(ctx context.Context) error
 }
